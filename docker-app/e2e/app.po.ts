03dd2648acf9305df930762370bc30b14557c929
Marius Stokseth <mariussto@netcompany.com>
2018-05-30 13:15:01 +0200

    updated angular project

--- docker-app/e2e/app.po.ts
+++ docker-app/e2e/app.po.ts
@@ -1,6 +1,6 @@
-import { browser, element, by } from 'protractor';
+import { browser, by, element } from 'protractor';
 
-export class MyappPage {
+export class AppPage {
   navigateTo() {
     return browser.get('/');
   }
